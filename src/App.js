76cbc82540db018bcaedf42d7a8a10ef45af6c82
minji <dylk97@gmail.com>
2023-05-14 06:33:56 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+import styled from "@emotion/styled";
+import "./App.css";
+
+import { Main } from "./Page/";
+
+const style = css`
+  color: hotpink;
+`;
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+  </div>
+);
+
+const anotherStyle = css({
+  textDecoration: "underline",
+});
+
+const AnotherComponent = () => (
+  <div css={anotherStyle}>Some text with an underline.</div>
+);
+
+const Button = styled.button`
+  color: hotpink;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SomeComponent>
+        <AnotherComponent />
+        <Button>This is a hotpink button.</Button>
+        <Main />
+      </SomeComponent>
     </div>
   );
 }
---
+++ src/Page/Main.jsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+import styled from "@emotion/styled";
+const jsonData = require("../example.json");
+
+let arr = [];
+for (let i = 0; jsonData.section.length >= i + 1; i++) {
+  for (let j = 0; jsonData.section[i].script.length >= j + 1; j++) {
+    for (let k = 0; jsonData.section[i].script[j].speech.length >= k + 1; k++) {
+      if (jsonData.section[i].script[j].type == "word") {
+        arr.push({
+          script: jsonData.section[i].script[j].speech[k],
+          character: jsonData.section[i].script[j].character,
+        });
+      }
+    }
+  }
+}
+
+const Main = () => {
+  return (
+    <>
+      {arr.map((a) => (
+        <div>{a.character}</div>
+      ))}
+    </>
+  );
+};
+
+export default Main;
